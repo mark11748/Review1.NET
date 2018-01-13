857f29515f8c89c0f978048865511cc3a3284d36
Mark Woodward <markwood117@gmail.com>
2018-01-12 17:12:56 -0800

    add cost to product class; update details, edit and create views for Product; add README.md; remove Ingredients from sidebar links;

--- Models/Product.cs
+++ Models/Product.cs
@@ -11,6 +11,7 @@ namespace Review1_NET.Models
         [Key]
         public int ProductId { get; set; }
         public string Name { get; set; }
+        public int Cost { get; set; }
         public string Description { get; set; }
         //public virtual ICollection<Ingredient> Ingredients { get; set; }
         
